61c26976d9223655c27f3a8040094977da61f3f6
Omar <ouardighiomar@gmail.com>
2024-10-25 12:26:03 +0200

    add the networking module for the Jenkins Instance

---
+++ tf-Jenkins/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    profile = "default"
+  region = "eu-west-1"
+}
+
+module "networking" {
+  source               = "./networking"
+  vpc_cidr             = var.vpc_cidr
+  vpc_name             = var.vpc_name
+  cidr_public_subnet   = var.cidr_public_subnet
+  eu_availability_zone = var.eu_availability_zone
+  cidr_private_subnet  = var.cidr_private_subnet
+}
