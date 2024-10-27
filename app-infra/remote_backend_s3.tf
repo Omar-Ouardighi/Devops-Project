b236245f2de5a5d45dbb0e57630c21924f08feaf
Omar <ouardighiomar@gmail.com>
2024-10-27 10:33:36 +0100

    create new s3 bucket

--- app-infra/remote_backend_s3.tf
+++ app-infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-123456"
+    bucket = "devops-proj-1-rs-bucket"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
